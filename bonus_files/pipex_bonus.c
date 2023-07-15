df0495a7b59381fda5907657c164e6dc46b59992
Dongkyu Lee <donglee2@c1r4s5.42seoul.kr>
2023-07-15 16:44:29 +0900

    here_doc null exception revised

--- bonus_files/pipex_bonus.c
+++ bonus_files/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: donglee2 <donglee2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 18:04:58 by donglee2          #+#    #+#             */
-/*   Updated: 2023/07/14 17:49:09 by donglee2         ###   ########seoul.kr  */
+/*   Updated: 2023/07/15 16:42:17 by donglee2         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,8 @@ int	make_tmp_here_doc_file(char *eof, t_args *args)
 	if (fd == -1)
 		exit(1);
 	new_line = get_next_line(0);
+	if (!new_line)
+		exit(0);
 	while (ft_strcmp(tmp, new_line))
 	{
 		ft_putstr_fd(new_line, fd);
